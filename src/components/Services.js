80620659311c05e79df33f50e05e987cda6f197f
geraldhalcyon <geraldsayson.halcyondigital@gmail.com>
2023-06-22 10:20:14 +0800

    initial commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -34,7 +34,7 @@ const Services = () => {
                         <Image className='w-[100%] h-[100%] object-cover'
                         src={slide.image.path} 
                         width={slide.image.width} height={slide.image.height} 
-                        alt={slide.image.alt} />
+                        alt={slide.image.alt} quality='50'/>
                     <div className={`text-center lg:text-left text-[var(--primary-color)] bg-black md:bg-inherit relative md:absolute md:bottom-[25px] left-[0px] bottom-0 lg:left-[25px] w-[100%] md:min-h-[auto] lg:w-[95%] lg:pr-[10px] px-[20px] py-[20px] lg:px-[0px] ${styles.imgFixheight}`}>
                         <h2 className='text-[23px] uppercase font-secondary font-black tracking-[2px]'>{slide.title}</h2>
                         <p className='text-[16px] leading-[27px] py-[16px]'>{slide.description} <span className='uppercase font-bold'>{slide.strongStyle}</span></p>
@@ -68,7 +68,7 @@ const Services = () => {
                 <div className='lg:col-span-10 relative'>
                     <Image className='w-[100%] md:h-[100%] h-auto object-cover'
                     src={servicesDetails.image.path} width={servicesDetails.image.width} height={servicesDetails.image.height} 
-                    alt={servicesDetails.image.alt} />
+                    alt={servicesDetails.image.alt} quality='50'/>
                     <div className='static md:absolute bottom-0 left-0 text-[var(--primary-color)] text-center lg:text-left block lg:flex justify-between w-[100%] py-[40px] px-[20px] bg-gradient-to-b from-black/100 via-black/100 to-black/100 md:bg-gradient-to-b md:from-black/0 md:via-black/50 md:to-black/60'>
                         <h1 className='uppercase tracking-[2px] font-secondary font-black drop-shadow-[0_0px_10px_rgba(0,0,0,0.25)] text-[23px] lg:mr-[20px] mr-[0px]]'>{servicesDetails.title}</h1>
                         <Link href={servicesDetails.path} target='_blank' className=' btn-primary py-[15px] px-[20px] hover:bg-[var(--fourth-color)] text-[13px] mt-[20px] lg:mt-[0px] block lg:flex lg:w-auto w-[200px] m-auto lg:m-[0]'>
