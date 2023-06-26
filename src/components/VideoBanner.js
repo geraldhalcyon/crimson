4608ccf793bd748e79c23b1e3c68aa3140e1afcc
geraldhalcyon <geraldsayson.halcyondigital@gmail.com>
2023-06-26 13:06:18 +0800

    initial commit

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -1,53 +1,46 @@
-import React, { useState,useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import styles from "../styles/banner.module.css";
 
-export const VideoBanner = ({block}) => {
- 
-  const [ready, setReady] = useState(false)
-  useEffect(() => { setTimeout(() => { setReady(true) }, 5000) }, [])
+export const VideoBanner = ({ block }) => {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setReady(true);
+    }, 10000);
+  }, []);
 
-  // style={{ backgroundImage: `url(${block.image.path})` }}
+  const [showVideo, setShowVideo] = useState(false);
+  useEffect(() => {
+    const delay = 10000; // Delay in milliseconds (e.g., 5000 = 5 seconds)
 
-  // const [showVideo, setShowVideo] = useState(false);
+    const timeout = setTimeout(() => {
+      setShowVideo(true);
+    }, delay);
 
-  // useEffect(() => {
-  //   const delay = 5000; // Delay in milliseconds (e.g., 5000 = 5 seconds)
+    return () => clearTimeout(timeout);
+  }, []);
 
-  //   const timeout = setTimeout(() => {
-  //     setShowVideo(true);
-  //   }, delay);
-
-  //   return () => clearTimeout(timeout);
-  // }, []);
-  
   return (
-   
-        <section className='mt-[87px] min-h-[200px] m-w-[100%] relative'>
-            <div className={`overflow-hidden bg-[#f5f5f5] relative ${styles.bannerContainer}`} style={{ backgroundImage: `url(${block.image.path})` }}>
-            
-            
-            <iframe loading="lazy"
-            className={`overflow-hidden md:absolute left-0 ${styles.bannerVideo}`}
-                autoPlay
-                scrolling="no"
+    <section className='mt-[87px] min-h-[200px] m-w-[100%] relative'>
+      <div
+        className={`overflow-hidden bg-[#f5f5f5] relative ${styles.bannerContainer}`}
+        style={{ backgroundImage: `url(${block.image.path})` }}
+      >
+        {ready && (
+          <>
+            {showVideo ? (
+              <iframe
+                className={`overflow-hidden md:absolute left-0 ${styles.bannerVideo}`}
+                loading="lazy"
+                allow="autoplay"
                 title='banner video'
-                src={ready ? block.video : ""}
-                // src={block.video}
-            />
-            {/* 
-            {showVideo && (
-            <iframe
-              className={`overflow-hidden md:absolute left-0 ${styles.bannerVideo}`}
-              loading="lazy"
-              allow="autoplay"
-              title='banner video'
-              src={block.video}
-            />
-          )} */}
+                src={block.video}
+              />
+            ) : null}
             <h1 className='uppercase text-center static md:absolute bottom-0 left-0 right-0 mt-[30px] md:mt-[0px] text-[var(--sixth-color)] md:text-[var(--primary-color)] font-secondary font-light tracking-[1px] text-[20px] md:text-[40px] px-[20px] md:pb-[10px] py-[20px] md:bg-gradient-to-b md:from-black/0 md:via-black/50 md:to-black/60'>{block.title}</h1>
-            </div>
-        </section>
-
-  )
-}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import Layout from '../components/Layout'
-// import Banner from '@/components/Banner';
-// import About from '@/components/About';
-// import Services from '@/components/Services';
 import homeData from '../static-data/home';
 import { Blocks } from '@/components/Blocks';
 
@@ -11,9 +8,6 @@ function Home() {
     <Layout>
       <main>
         <Blocks blocks={blocks} />
-        {/* <Banner />
-        <About />
-        <Services /> */}
       </main>
     </Layout>
   )
