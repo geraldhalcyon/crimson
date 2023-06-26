7f6680fdcc55afecb739dd1c347272360ebbe514
geraldhalcyon <geraldsayson.halcyondigital@gmail.com>
2023-06-26 10:03:46 +0800

    initial commit

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -49,6 +49,7 @@ export const VideoBanner = ({block}) => {
             loading="lazy"
             allow="autoplay"
             title='banner video'
+            defer
             src={block.video} />
             <h1 className='uppercase text-center static md:absolute bottom-0 left-0 right-0 mt-[30px] md:mt-[0px] text-[var(--sixth-color)] md:text-[var(--primary-color)] font-secondary font-light tracking-[1px] text-[20px] md:text-[40px] px-[20px] md:pb-[10px] py-[20px] md:bg-gradient-to-b md:from-black/0 md:via-black/50 md:to-black/60'>{block.title}</h1>
             </div>
